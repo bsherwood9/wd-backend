89addd96ad15edf98202b748c550116693490285
bsherwood9 <36565536+bsherwood9@users.noreply.github.com>
2020-05-27 14:10:51 -0600

    Got the SQL query to return what we needed.

--- models/words-model.js
+++ models/words-model.js
@@ -15,30 +15,34 @@ function addExamples(examp, word_id) {
 }
 
 function findWordsDefsExamples(user_id) {
-  const length = () => {
-    return db("users_words as uw")
-      .join("users as u", "u.id", "uw.user_id")
-      .join("words as w", "w.id", "uw.word_id")
-      .select(db.raw("ARRAY_LENGTH(array_agg(distinct w.word),1)"))
-      .where({ user_id: user_id });
-  };
-  let realLength = length();
-  if (realLength) {
-    console.log("this is the length", realLength[0].array_length);
-    return db("users_words as uw")
-      .join("users as u", "u.id", "uw.user_id")
-      .join("words as w", "w.id", "uw.word_id")
-      .join("definitions as d", "d.word_id", "w.id")
-      .join("examples as e", "e.word_id", "w.id")
-      .select(
-        db.raw("ARRAY_LENGTH(array_agg(distinct w.word),1)"),
-        db.raw("array_agg(w.word)")
-        // db.raw("ARRAY_AGG(distinct e.text) as Examples"),
-        // db.raw("ARRAY_AGG(distinct d.text) as Definitions")
+  // const length = () => {
+  //   return db("users_words as uw")
+  //     .join("users as u", "u.id", "uw.user_id")
+  //     .join("words as w", "w.id", "uw.word_id")
+  //     .select(db.raw("ARRAY_LENGTH(array_agg(distinct w.word),1)"))
+  //     .where({ user_id: user_id });
+  // };
+  // let realLength = length();
+  // if (realLength) {
+  // console.log("this is the length", realLength[0].array_length);
+  return db("users_words as uw")
+    .join("users as u", "u.id", "uw.user_id")
+    .join("words as w", "w.id", "uw.word_id")
+    .join("definitions as d", "d.word_id", "w.id")
+    .join("examples as e", "e.word_id", "w.id")
+    .select(
+      // db.raw("array_agg(distinct w.word) as Words"),
+      // db.raw("ARRAY_AGG(distinct e.text) as Examples"),
+      // db.raw("ARRAY_AGG(distinct d.text) as Definitions")
+
+      db.raw(
+        "w.word, array_agg(distinct e.text)as examples, array_agg(distinct d.text) as definitions"
       )
-      .where({ user_id: user_id });
-  }
+    )
+    .groupBy("w.word")
+    .where({ user_id });
 }
+
 // .union([
 //   db("users_words as uw")
 //     .join("users as u", "u.id", "uw.user_id")
